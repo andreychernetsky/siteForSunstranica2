9c0754ce7a8ef0a348c473e5f627889c65db303d
andric <you@example.com>
2018-12-06 19:19:21 +0300

    burger

--- js/sstu_script.js
+++ js/sstu_script.js
@@ -9,14 +9,16 @@ $(document).ready(function(){
   merge:true,
   slideTransition:'.3s'
   });
-  $(".btn-nav").on("click", function(){
-		var target = $(this).data("target");
-		$(target).toggleClass("nav__list--open")
-	});
+  
 
 });
 
-// jQuery(function($) {
-	
-// });
+$(document).ready(function () {
+  $('a').attr('target', 'blank');
+  $('.social-button').click(function () {
+    $('.social').toggle(500);
+    $('.icon-bar').toggleClass('open');
+  })
+})
+
 
